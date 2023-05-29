bbd825aa091d863d89d1a6b51ab7b5fb329eef0e
Chirag Chaplot <chirag.chaplot@aware.com.au>
2023-05-29 15:52:08 +1000

    Initial Commit

---
+++ CovidStats/CovidStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidStatsApp.swift
+//  CovidStats
+//
+//  Created by Chirag Chaplot on 29/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct CovidStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
